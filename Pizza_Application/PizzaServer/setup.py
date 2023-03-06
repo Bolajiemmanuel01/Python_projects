cf9066acb33017505641a6d8979b533942aa7bad
Bolajiemmanuel01 <Bolajiemmanuel01@gmail.com>
2023-03-06 12:57:40 +0100

    Uploading the final packaged application

---
+++ Pizza_Application/PizzaServer/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='PizzaServer',
+    version='1.0',
+    packages=['pizzaserver'],
+    package_data={'': ['pizza.db']},
+    url='www.pythonpizza.biz',
+    license='',
+    author='Bolaji_Emmanuel',
+    author_email='',
+    description='Pizza Calculator App'
+)
